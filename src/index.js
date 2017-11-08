9a1134512c939fa3f94d9cdd648feda04f72a109
Jacob Adlers <jacob.adlers@gmail.com>
2017-11-09 02:45:26 +1100

    Make board size easily updated

--- src/index.js
+++ src/index.js
@@ -13,29 +13,29 @@ function Square(props) {
 class Board extends React.Component {
   renderSquare(i) {
     const { squares, onClick } = this.props;
-    return <Square value={squares[i]} onClick={() => onClick(i)} />;
+    return <Square key={i} value={squares[i]} onClick={() => onClick(i)} />;
+  }
+
+  createRow(startNum, columns) {
+    const squares = [];
+    for (var i = 0; i < columns; i++) {
+      squares.push(this.renderSquare(startNum++));
+    }
+    return (
+      <div key={startNum} className="board-row">
+        {squares}
+      </div>
+    );
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    const boardSize = 3;
+    const rows = [];
+    for (var i = 0; i < boardSize; i++) {
+      rows.push(this.createRow(i * boardSize, boardSize));
+    }
+
+    return <div>{rows}</div>;
   }
 }
 

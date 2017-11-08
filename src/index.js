2c456d368cf2fc257d47212048f64eebec74bc79
Jacob Adlers <jacob.adlers@gmail.com>
2017-11-09 02:13:16 +1100

    Make step looked at be bold

--- src/index.js
+++ src/index.js
@@ -98,9 +98,13 @@ class Game extends React.Component {
         ? "Go to move #" + move + " " + this.getCoordinates(step.updatedSquare)
         : "Go to game start";
 
+      const lookedAt = move === this.state.stepNumber ? "bold" : "";
+
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button className={lookedAt} onClick={() => this.jumpTo(move)}>
+            {desc}
+          </button>
         </li>
       );
     });

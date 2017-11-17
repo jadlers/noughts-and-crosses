02a7e5decc421f8ce1d724d3b7f5f5397ee3c08b
Jacob Adlers <jacob.adlers@gmail.com>
2017-11-17 23:49:27 +1100

    Add correct vertical lines

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,8 @@
 export function calculateWinner(squares) {
-  const lines = validHorizontal(squares);
+  let lines = validHorizontal(squares);
+  validVertical(squares).forEach(element => {
+    lines.push(element);
+  });
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
@@ -27,6 +30,30 @@ function horizontalSequence(start) {
   const seqLength = 3;
   let res = [];
   for (let i = start; i < start + seqLength; i++) {
+    res.push(i);
+  }
+  return res;
+}
+
+function validVertical(squares, columns = 3) {
+  const seqLength = 3;
+  const numSquares = squares.length;
+  let validRows = [];
+  for (let i = 0; i < numSquares; i++) {
+    const lastSquare = i + (seqLength - 1) * columns;
+    if (lastSquare < numSquares) {
+      validRows.push(i);
+    }
+  }
+
+  return validRows.map(verticalSequence);
+}
+
+function verticalSequence(start) {
+  const seqLength = 3;
+  const rowLength = 3;
+  let res = [];
+  for (let i = start; i < start + seqLength * rowLength; i += rowLength) {
     res.push(i);
   }
   return res;

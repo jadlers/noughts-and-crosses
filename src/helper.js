334fd0b1d2d4cb293dddd0f195a7e9e039b90700
Jacob Adlers <jacob.adlers@gmail.com>
2017-11-25 13:25:22 +1000

    Add diagonal skeleton

--- src/helper.js
+++ src/helper.js
@@ -3,6 +3,9 @@ export function calculateWinner(squares) {
   validVertical(squares).forEach(element => {
     lines.push(element);
   });
+  validDiagonal(squares).forEach(line => {
+    lines.push(line);
+  });
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
@@ -54,6 +57,48 @@ function verticalSequence(start) {
   const rowLength = 3;
   let res = [];
   for (let i = start; i < start + seqLength * rowLength; i += rowLength) {
+    res.push(i);
+  }
+  return res;
+}
+
+function validDiagonal(squares, columns = 3) {
+  let res = [];
+
+  // Slash
+  for (let i = 0; i < squares.length; i++) {
+    if (false) {
+      res.push(slashSequence(i));
+    }
+  }
+
+  // Backslash
+  for (let i = 0; i < squares.length; i++) {
+    if (false) {
+      res.push(backSlashSequence(i));
+    }
+  }
+
+  return res;
+}
+
+function backSlashSequence(start) {
+  const seqLength = 3;
+  const rowLength = 3;
+  const lastIndex = start + (rowLength + 1) * (seqLength - 1);
+  let res = [];
+  for (let i = start; i <= lastIndex; i += rowLength + 1) {
+    res.push(i);
+  }
+  return res;
+}
+
+function slashSequence(start) {
+  const seqLength = 3;
+  const rowLength = 3;
+  const lastIndex = start + (rowLength - 1) * (seqLength - 1);
+  let res = [];
+  for (let i = start; i <= lastIndex; i += rowLength - 1) {
     res.push(i);
   }
   return res;

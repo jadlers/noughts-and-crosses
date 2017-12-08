b17a33bfcd2e450ec3b1fb718c9fc25f04a82fdc
Jacob Adlers <jacob.adlers@gmail.com>
2017-12-08 19:59:54 +1000

    Wrap Game in App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Game from "./Game.js";
+import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));

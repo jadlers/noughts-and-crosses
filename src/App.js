cba4f1e78a8ff0a0c2ccf87ffd8637dadf1a74fa
Jacob Adlers <jacob.adlers@gmail.com>
2017-12-19 18:05:43 +0800

    Make App a pure functional component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Game from './Game';
 
-class App extends React.Component {
-  render() {
-    const title = 'Noughts & crosses';
-    return (
-      <div>
-        <h1>{title}</h1>
-        <Game />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const title = 'Noughts & crosses';
+  return (
+    <div>
+      <h1>{title}</h1>
+      <Game />
+    </div>
+  );
+};
 
 export default App;

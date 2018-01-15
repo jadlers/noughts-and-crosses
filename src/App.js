850d127f966f781d930638ffa1f5bb17149ca44f
Jacob Adlers <jacob.adlers@gmail.com>
2018-01-15 21:52:17 +0100

    Add Router to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
 import Game from './Game';
 
 const App = () => {
   const title = 'Noughts & crosses';
   return (
-    <div>
-      <h1>{title}</h1>
-      <Game />
-    </div>
+    <Router>
+      <div>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <h1>{title}</h1>
+        </Link>
+        <Switch>
+          <Route path="/play" component={Game} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
